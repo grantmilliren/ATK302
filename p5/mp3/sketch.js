45af27cbd342b4352d33218ff56eed87da788d8f
Grant Milliren <gmillir@ilstu.edu>
2019-10-28 17:08:52 -0500

    yo

--- p5/mp3/sketch.js
+++ p5/mp3/sketch.js
@@ -2,9 +2,7 @@ var cars = [];
 var frogPos;
 var myState = 0;
 var timer = 0;
-var yoda, yodaLeft, yodaRight;
-var bird ;
-var birds = [];
+var dogfood, dogLeft, dogRight;
 
 function setup() {
 
@@ -15,14 +13,13 @@ function setup() {
   for (var i = 0; i < 5; i++) {
     cars.push(new Car());
   }
-  dogLeft = loadImage("assets/Dog.jpeg") ;
-  dogRight = loadImage("assets/Dog2.jpeg");
-  dog = dogLeft ;
 
-  dogfood = loadImage("assets/DogFood.jpg");
-  birds[0] = loadImage("assets/bird1.png");
-  birds[1] = loadImage("assets/bird2.png");
-  birds[2] = loadImage("assets/bird3.png");
+  dogLeft = loadImage("assets/Dog.png");
+  dogRight = loadImage("assets/Dog2.png");
+  dog = dogLeft;
+
+  dogfood = loadImage("assets/DogFood.png");
+  goldendog = loadImage("assets/goldendog.png");
 
 
 
@@ -37,15 +34,16 @@ function draw() {
   switch (myState) {
 
     case 0: // splash screen
-      background('orange');
+      background('blue');
+      image(goldendog, width / 2, height / 2);
       textSize(60);
-      text('WELCOME TO \n THE GAME', 100, 100);
+      text('WELCOME TO THE GAME. \n EAT ALL THE DOG FOOD!', 25, 100);
       break;
 
     case 1: // the game state
       game();
       timer++;
-      if (timer > 600) {
+      if (timer > 900) {
 
         myState = 3;
         timer = 0;
@@ -53,17 +51,17 @@ function draw() {
       break;
 
     case 2: // the win state
-      background('orange');
-      fill('purple', 100, 100);
+      background('Blue');
+      fill('black', 100, 100);
       textSize(60);
-      text('YOU WON!', 100, 100);
+      text('You won! Play Again', 100, 100);
       break;
 
     case 3: // the lose state
-      background('orange');
-      fill('red');
+      background('red');
+      fill('black', 100, 100);
       textSize(60);
-      text('YOU DONE!', 100, 100);
+      text('You lost! \nTry better next time!', 100, 100);
       break;
 
   }
@@ -96,7 +94,7 @@ function Car() {
   this.r = random(255);
   this.g = random(255);
   this.b = random(255);
-  this.birdNum = 0;
+  //  this.birdNum = 0;
   this.timer = 0;
   // methods
   this.display = function() {
@@ -105,18 +103,10 @@ function Car() {
     // ellipse(this.pos.x - 45, this.pos.y + 25, 50, 50);
     // ellipse(this.pos.x + 45, this.pos.y + 25, 50, 50);
     //image(bird, this.pos.x, this.pos.y, 100, 100);
-image(birds[this.birdNum], this.pos.x, this.pos.y, 100, 100);
+    image(dogfood, this.pos.x, this.pos.y, 100, 100);
 
-  this.timer++;
-  if(this.timer > 20){
-    this.birdNum = this.birdNum + 1;
-    this.timer = 0;
   }
 
-  //
-  if (this.birdNum > birds.length - 1) //could also put > 2
-    this.birdNum = 0;
-  }
 
   this.drive = function() {
     this.pos.add(this.vel);
@@ -131,21 +121,21 @@ image(birds[this.birdNum], this.pos.x, this.pos.y, 100, 100);
 }
 
 function keyPressed() {
-  if (keyCode === LEFT_ARROW) yoda =yodaLeft ;
-  if (keyCode === RIGHT_ARROW) yoda =yodaRight ;
+  if (keyCode === LEFT_ARROW) dog = dogLeft;
+  if (keyCode === RIGHT_ARROW) dog = dogRight;
 
 }
 
 function checkForKeys() {
-  if (keyIsDown(LEFT_ARROW)) frogPos.x = frogPos.x - 5;
-  if (keyIsDown(RIGHT_ARROW)) frogPos.x = frogPos.x + 5;
-  if (keyIsDown(UP_ARROW)) frogPos.y = frogPos.y - 5;
-  if (keyIsDown(DOWN_ARROW)) frogPos.y = frogPos.y + 5;
+  if (keyIsDown(LEFT_ARROW)) frogPos.x = frogPos.x - 15;
+  if (keyIsDown(RIGHT_ARROW)) frogPos.x = frogPos.x + 15;
+  if (keyIsDown(UP_ARROW)) frogPos.y = frogPos.y - 15;
+  if (keyIsDown(DOWN_ARROW)) frogPos.y = frogPos.y + 15;
 
 }
 
 function resetTheGame() {
-  car = [];
+  cars = [];
   for (var i = 0; i < 10; i++) {
     cars.push(new Car());
   }
@@ -164,11 +154,11 @@ function game() {
   }
   if (cars.length == 0) {
     myState = 2;
-    frogPos = 0;
+    // you were resetting frogPos to 0 here.
   }
   // draw the frog
   fill('green');
   //ellipse(frogPos.x, frogPos.y, 60, 60);
-  image(yoda, frogPos.x, frogPos.y);
+  image(dog, frogPos.x, frogPos.y);
   checkForKeys();
 }
